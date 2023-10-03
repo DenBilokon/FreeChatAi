c3d18c910e2cc7ba8823195d1125aff83cf6449c
Sanyavas <vasilinaoleksandr@gmail.com>
2023-10-03 09:02:06 +0300

    hz

--- src/services/doc_handler.py
+++ src/services/doc_handler.py
@@ -12,7 +12,7 @@ from htmlTemplates import bot_template, user_template, css
 
 
 def get_pdf_text(pdf_files):
-    
+
     text = ""
     for pdf_file in pdf_files:
         reader = PdfReader(pdf_file)
@@ -20,8 +20,9 @@ def get_pdf_text(pdf_files):
             text += page.extract_text()
     return text
 
+
 def get_chunk_text(text):
-    
+
     text_splitter = CharacterTextSplitter(
     separator = "\n",
     chunk_size = 1000,
@@ -35,21 +36,22 @@ def get_chunk_text(text):
 
 
 def get_vector_store(text_chunks):
-    
+
     # For OpenAI Embeddings
-    
+
     embeddings = OpenAIEmbeddings()
-    
+
     # For Huggingface Embeddings
 
     # embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2')
 
-    vectorstore = FAISS.from_texts(texts = text_chunks, embedding = embeddings)
-    
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+
     return vectorstore
 
+
 def get_conversation_chain(vector_store):
-    
+
     # OpenAI Model
 
     llm = ChatOpenAI()
@@ -67,6 +69,7 @@ def get_conversation_chain(vector_store):
     )
 
     return conversation_chain
+
 
 def handle_user_input(question):
 
@@ -86,19 +89,19 @@ def main():
     st.set_page_config(page_title='Chat with Your own PDFs', page_icon=':books:')
 
     st.write(css, unsafe_allow_html=True)
-    
+
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
-    
+
     st.header('Chat with Your own PDFs :books:')
     question = st.text_input("Ask anything to your PDF: ")
 
     if question:
         handle_user_input(question)
-    
+
 
     with st.sidebar:
         st.subheader("Upload your Documents Here: ")
@@ -112,10 +115,10 @@ def main():
 
                 # Get Text Chunks
                 text_chunks = get_chunk_text(raw_text)
-                
+
 
                 # Create Vector Store
-                
+
                 vector_store = get_vector_store(text_chunks)
                 st.write("DONE")
 
